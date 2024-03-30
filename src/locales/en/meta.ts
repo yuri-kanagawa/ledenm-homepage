80d4a0b5577d9c86a9eba40c8e52f05c4bd1b1a2
yuri-kanagawa <nightmare37998@icloud.com>
2024-03-31 07:58:40 +0900

    meta 修正

--- src/locales/en/meta.ts
+++ src/locales/en/meta.ts
@@ -8,6 +8,11 @@ export const meta: Metadata = {
   description: DESCRIPTION,
   openGraph: {
     title: LEDENM,
-    description: DESCRIPTION
+    description: DESCRIPTION,
+    images: '/opengraph-image.png'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: '/twitter-image.png'
   }
 }
