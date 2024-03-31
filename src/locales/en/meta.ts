fa72aefa8a1838d3d2a7214ab7b8fb5d744b6c1e
yuri-kanagawa <nightmare37998@icloud.com>
2024-03-31 11:21:46 +0900

    url 修正

--- src/locales/en/meta.ts
+++ src/locales/en/meta.ts
@@ -1,18 +1,18 @@
 import { Metadata } from 'next'
-import { BASE_URL, LEDENM, OGP_IMAGE_URL } from 'src/constants/ledenm'
+import { BASE_URL, LEDENM } from 'src/constants/ledenm'
 import ogImage from 'src/app/opengraph-image.png'
 
 const DESCRIPTION = 'Introduction to applications created by ledenm'
-const LOCALE_URL = BASE_URL
 
 export const meta: Metadata = {
   title: LEDENM,
   description: DESCRIPTION,
+  metadataBase: new URL(BASE_URL),
   openGraph: {
     title: LEDENM,
     description: DESCRIPTION,
     images: [{
-      url: OGP_IMAGE_URL,
+      url: ogImage.src,
       width: ogImage.width,
       height: ogImage.height
 
@@ -22,7 +22,7 @@ export const meta: Metadata = {
     card: 'summary_large_image',
     images: [
       {
-        url: OGP_IMAGE_URL,
+        url: ogImage.src,
         width: ogImage.width,
         height: ogImage.height
       }
